bd87d304851f29a6f5a99a9a0d10e7388720fc13
Erick Santana <120190518+ErickDevp@users.noreply.github.com>
2023-04-03 11:18:02 -0300

    correção de erros

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,13 +6,11 @@ const navClose = document.querySelector("ul")
 
 
 
-navClose.addEventListener("click", () => {        
-        
-    menuSection.classList.remove("on")
-})
-
 menuToggle.addEventListener("click", () => {
-    
+    navClose.addEventListener("click", () => {        
+        menuSection.classList.remove("on")
+    })
+        
     menuSection.classList.toggle("on", show)
     show = !show;
 })
